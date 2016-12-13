f8e7ada4b901273506ff7a8389fecef2c7b4fdc5
PiotrBerebecki <petebere@gmail.com>
2016-12-13 15:44:56 +0000

    Update app structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      soundSrc: [
+        { 
+          id: 'one',
+          src: '../public/assets/image/'
+        }
+      ]
+    };
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section id="drumkit">
+        <Button />
+      </section>
     );
   }
 }
