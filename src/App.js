4ee1af75d57aa616aff89213c5d7cde9d7143b70
PiotrBerebecki <petebere@gmail.com>
2016-12-16 16:05:22 +0000

    Move sounds to a separate file

--- src/App.js
+++ src/App.js
@@ -1,70 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from './Button.js';
+import { sounds } from './sounds';
 
 class App extends Component {
   constructor() {
     super();
     this.playSound = this.playSound.bind(this);
     this.state = {
-      sounds: [
-        { 
-          imageSrc: './assets/image/crash.png',
-          audioSrc: './assets/sound/crash.mp3',
-          isDarkText: false,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/drum.png',
-          audioSrc: './assets/sound/big-rack-tom.mp3',
-          isDarkText: true,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/drum.png',
-          audioSrc: './assets/sound/tom.wav',
-          isDarkText: true,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/crash.png',
-          audioSrc: './assets/sound/ride.wav',
-          isDarkText: false,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/hihat.png',
-          audioSrc: './assets/sound/openhat.wav',
-          isDarkText: false,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/drum.png',
-          audioSrc: './assets/sound/snare.mp3',
-          isDarkText: true,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/drum.png',
-          audioSrc: './assets/sound/kick.mp3',
-          isDarkText: true,
-          playing: false,
-          audioObj: null
-        },
-        { 
-          imageSrc: './assets/image/drum.png',
-          audioSrc: './assets/sound/floor-tom.mp3',
-          isDarkText: true,
-          playing: false,
-          audioObj: null
-        }
-      ]
+      sounds: sounds
     };
   }
   
